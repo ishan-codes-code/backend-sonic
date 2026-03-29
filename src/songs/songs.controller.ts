e75d940f1fa520a32cd5cd02bb0a935f93d4b50c
ishan-codes-code <srivastava.ishan.work@gmail.com>
2026-03-30 04:05:43 +0530

    Add song processing jobs and project documentation

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,4 +1,14 @@
-import { BadRequestException, Body, Controller, Get, Param, Post, Sse, UseGuards, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { SongsService } from './songs.service';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { SongDto } from './dto/song.dto';
@@ -6,22 +16,47 @@ import { R2Service } from 'src/r2/r2.service';
 
 @Controller('songs')
 export class SongsController {
-    constructor(
-        private readonly songsService: SongsService,
-        private readonly r2Service: R2Service,
-    ) { }
+  constructor(
+    private readonly songsService: SongsService,
+    private readonly r2Service: R2Service,
+  ) {}
 
-    @UseGuards(JwtAuthGuard)
-    @Post('play')
-    async getSong(@Body() songDto: SongDto) {
-        return this.songsService.play(songDto);
+  @UseGuards(JwtAuthGuard)
+  @Post('play')
+  async getSong(@Body() songDto: SongDto) {
+    return this.songsService.play(songDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('play/:songId')
+  async getUrl(@Param('songId') songId: string) {
+    const song = await this.songsService.findById(songId);
+    if (!song) {
+      throw new HttpException(
+        'Song not found in global storage',
+        HttpStatus.NOT_FOUND,
+      );
     }
 
-    @UseGuards(JwtAuthGuard)
-    @Get('getAll')
-    async getAllSongs() {
-        return this.songsService.getAllSongs();
+    const streamUrl = await this.r2Service.getSignedUrl(song.r2Key);
+    return { streamUrl };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('job/:jobId')
+  async getSongJob(@Param('jobId') jobId: string) {
+    const jobStatus = this.songsService.getJobStatus(jobId);
+
+    if (!jobStatus) {
+      throw new NotFoundException('Job not found');
     }
 
+    return jobStatus;
+  }
 
+  @UseGuards(JwtAuthGuard)
+  @Get('getAll')
+  async getAllSongs() {
+    return this.songsService.getAllSongs();
+  }
 }
--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -3,9 +3,10 @@ import { SongsService } from './songs.service';
 import { SongsController } from './songs.controller';
 import { SongFilesModule } from 'src/song-files/song-files.module';
 import { R2Module } from 'src/r2/r2.module';
+import { JobService } from './job.service';
 
 @Module({
-  providers: [SongsService],
+  providers: [SongsService, JobService],
   exports: [SongsService],
   controllers: [SongsController],
   imports: [SongFilesModule, R2Module]
