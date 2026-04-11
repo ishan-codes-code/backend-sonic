15f294b3c3c380bb95539b8b4cd8b6b5e91852cd
ishan-codes-code <srivastava.ishan.work@gmail.com>
2026-04-12 02:28:53 +0530

    feat: enable JwtAuthGuard for playback endpoints

--- src/modules/songs/songs.controller.ts
+++ src/modules/songs/songs.controller.ts
@@ -9,25 +9,23 @@ import {
 import { SongsService } from './songs.service';
 import { JwtAuthGuard } from '../../infrastructure/common/guards/jwt-auth.guard';
 import { PlaySongDto } from './dto/song.dto';
-import { SongStreamService } from './song-stream.service';
 
 @Controller('songs')
 export class SongsController {
   constructor(
     private readonly songsService: SongsService,
-    private readonly songStreamService: SongStreamService,
   ) { }
 
 
 
-  // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Post('play')
   async playSongResolver(@Body() dto: PlaySongDto) {
     return this.songsService.play(dto);
 
   }
 
-  // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Get('job/:jobId')
   async getSongJob(@Param('jobId') jobId: string) {
     return this.songsService.getJobStatus(jobId);
